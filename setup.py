5ba3e9fb1b9d65f78274f47bde35d3ca6cdb5be2
Per August Moen <peraugust.pa@gmail.com>
2023-07-14 13:26:18 +0200

    hei

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, Extension
 import numpy
-setup(
-    name="fastperm",
-    version="0.1",
-    include_dirs=[numpy.get_include()],
-    ext_modules=[Extension("fastperm", ["get_log_permanent.c", "help_functions.c", "get_alphabetagamma.c",\
-                "methods_sparse.c", "methods.c", "xxhash.c"])]
-)
+
+def main():
+    setup(
+        name="fastperm",
+        version="0.1",
+        include_dirs=[numpy.get_include()],
+        ext_modules=[Extension("fastperm", ["get_log_permanent.c", "help_functions.c", "get_alphabetagamma.c",\
+                "methods_sparse.c", "methods.c", "xxhash.c"])])
+
+if __name__ == "__main__":
+    main()

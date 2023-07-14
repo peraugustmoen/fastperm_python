830ef9d7d8cb5017718360d27d48c84cc6f63929
Per August Moen <peraugust.pa@gmail.com>
2023-07-14 14:26:51 +0200

    hei

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ def main():
         version="0.1",
         include_dirs=[numpy.get_include()],
         ext_modules=[Extension("fastperm", ["get_log_permanent.c", "help_functions.c", "get_alphabetagamma.c",\
-                "methods_sparse.c", "methods.c", "xxhash.c"])])
+                "methods_sparse.c", "methods.c", "xxhash.c"])],
+        headers = ["xxhash.h", "header.h"])
 
 if __name__ == "__main__":
     main()

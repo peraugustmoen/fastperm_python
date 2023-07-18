895524d88e2f2372672f576728d48fd359feeb4a
Per August Moen <peraugust.pa@gmail.com>
2023-07-18 02:06:56 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ def main():
     setup(
         license='MIT',
         description = 'The Fastperm package implements the algorithm proposed by Christensen (Inference for Bayesian Nonparametric Models with Binary Response Data via Permutation Counting, Bayesian Analysis, 1(1), 1-26,2023) for computing the permanent of a block rectangular matrix.',
-        author = 'Dennis Christensen, Per August Jarval Moen',                   # Type in your name
-        author_email = 'Dennis.Christensen@ffi.no, pamoen@math.uio.no',      # Type in your E-Mail
-        url = 'https://github.com/pamoen/fastperm',   # Provide either the link to your github or to your website
-        download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+        author = 'Dennis Christensen, Per August Jarval Moen',                  
+        author_email = 'Dennis.Christensen@ffi.no, pamoen@math.uio.no',      
+        url = 'https://github.com/peraugustmoen/fastperm',  
+        download_url = 'https://github.com/peraugustmoen/fastperm/archive/refs/tags/v_01.tar.gz',
         name="fastperm",
         packages=["fastperm"],
         version="0.1",

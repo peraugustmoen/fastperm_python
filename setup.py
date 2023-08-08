e0889c259794b472c8ce2de7b36ec5b014567245
Per August Moen <peraugust.pa@gmail.com>
2023-08-08 15:51:57 +0200

    extra

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ def main():
         author = 'Dennis Christensen, Per August Jarval Moen',                  
         author_email = 'Dennis.Christensen@ffi.no, pamoen@math.uio.no',      
         url = 'https://github.com/peraugustmoen/perms',  
-        download_url = 'https://github.com/peraugustmoen/fastperm/archive/refs/tags/v_1.tar.gz',
+        download_url = 'https://github.com/peraugustmoen/fastperm/archive/refs/tags/v_1_1.tar.gz',
         name="fastperm",
-        version="1.0",
+        version="1.1",
         install_requires=["numpy"],
         include_dirs=[np.get_include()],
         ext_modules=[Extension("perms", ["get_log_permanent.c", "help_functions.c", "get_alphabetagamma.c",\

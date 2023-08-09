0238429dded571e99f92ac8af45f3ef6fd28e60c
Per August Moen <peraugust.pa@gmail.com>
2023-08-09 14:07:43 +0200

    license fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def main():
     setup(
-        license='MIT',
+        license='BSD-2-Clause',
         description = 'The perms package implements the algorithm proposed by Christensen (Inference for Bayesian Nonparametric Models with Binary Response Data via Permutation Counting, Bayesian Analysis, 1(1), 1-26,2023) for computing the permanent of a block rectangular matrix.',
         author = 'Dennis Christensen, Per August Jarval Moen',                  
         author_email = 'Dennis.Christensen@ffi.no, pamoen@math.uio.no',      
